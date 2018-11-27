08dd487500b572df0f38dc83c0c9cae127521c3a
shivani brijmohan <sbrij001@yahoo.com>
2018-11-27 19:52:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 katz_deli = []
 
-#create a method #line that takes in an array 
-#The method should return a string 
+#create a method #line that takes in an array
+#The method should return a string
 # if the line array is empty #line should say the line is empty
 # if there are people in the line #line should display the current line + the name and it's (index + 1)
 #use the each_with_index to iterate thru the array
-#convert the array into a string before returning 
+#convert the array into a string before returning
 def line(katz_deli)
   new_arr = []
     if katz_deli.length > 0
@@ -20,11 +20,11 @@ def line(katz_deli)
 end
 
 
-#take_a_number should accept 2 arguments katz_deli and a string 
-#return the name along with their position in line 
-#if #take_a_number is empty it needs to add a person to the line 
-#if there are already people in the line #take_a_number should add a person to the end of the line 
-#needs to be able to add mult people in a row 
+#take_a_number should accept 2 arguments katz_deli and a string
+#return the name along with their position in line
+#if #take_a_number is empty it needs to add a person to the line
+#if there are already people in the line #take_a_number should add a person to the end of the line
+#needs to be able to add mult people in a row
 #tip  Remember that people like to count from 1, not from 0 ("zero") like computers.
 
 
